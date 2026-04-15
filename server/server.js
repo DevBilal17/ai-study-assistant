d752f18d27e329855acbf6bc479ee3d850c150d6
DevBilal17 <muhammadbilalmb03122@gmail.com>
2026-04-15 20:42:26 +0500

    Connection to DB, Server Creation Done

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const dotenv = require("dotenv")
+const app = require("./app")
+const connectDB = require("./config/db")
+const PORT = process.env.PORT || 5000
+
+dotenv.config()
+
+connectDB()
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
+
+
